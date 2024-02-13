177e8c15bf4e74311be086c1de1b8f6bc6fccf82
Sarvin <sarvin.rezazadeh7979@gmail.com>
2024-02-14 00:53:26 +0330

    add mvc persian phonebook

---
+++ MVCphoneBook/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MVCphoneBook.MvcApplication" Language="C#" %>
---
+++ MVCphoneBook/Models/EntityModels/TblUsers.Metadata.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MVCphoneBook.Models.EntityModels
+{
+    public class TblUsers
+    {
+       
+        public int ID { get; set; }
+        [Display(Name = "نام")]
+        public string Name { get; set; }
+        [Display(Name = " نام خانوادگی")]
+        public string FamilyName { get; set; }
+        [Display(Name = "تلفن همراه")]
+        public string Mobile { get; set; }
+        [Display(Name = "تلفن ثابت")]
+        public string PhoneNumber { get; set; }
+        [Display(Name = "کد ملی")]
+        public string NationalCode { get; set; }
+        [Display(Name = "موقعیت شغلی")]
+        public string OrgPos { get; set; }
+    }
+}
+
+namespace MVCphoneBook.Models.DominModels
+{
+    [MetadataType(typeof(EntityModels.TblUsers))]
+    public partial class TblUser
+    {
+
+    }
+}
